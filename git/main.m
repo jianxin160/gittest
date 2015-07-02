d481271863dfe5d50ad1870994b041b587f0c533
Kenshin Cui <kenshincui@hotmail.com>
2015-07-02 13:37:34 +0800

    init

---
+++ git/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  git
+//
+//  Created by KenshinCui on 15/7/2.
+//  Copyright (c) 2015年 Jinritemai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
---
+++ readme.txt
@@ -0,0 +1,11 @@
+1.查看git
+cat ~/.gitconfig
+2.配置git
+git config --global user.name
+git config --global user.mail
+3.查看常用命令(q退出查看)
+git help <command>
+4.给项目添加git版本库（本地版本库）
+git init
+git add
+git commit -m "init"
